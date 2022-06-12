fc16c96cc500082a4aca0fed75bfd7be64f448b0
Nathan Campos <nathan@innoveworkshop.com>
2022-06-12 12:42:10 +0100

    Started working on the bedrock

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * config.php
+ * The application configuration file.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+define('APP_NAME', 'PickLE');
+define('SITE_URL', 'http://localhost/~nathanpc/pickle');
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<?php require __DIR__ . "/../config/config.php"; ?>
+	<head>
+		<meta charset="utf-8" />
+		<title><?= APP_NAME ?></title>
+	</head>
+	<body>
+		<h1>Welcome to <?= APP_NAME ?></h1>
+	</body>
+</html>
