b7d308925fd4158802b9f9304b245cd0c8efa448
Nathan Campos <nathan@innoveworkshop.com>
2022-07-01 12:32:59 +0100

    Implemented #3 - Allow the user to 'pick off' reference designators

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,12 +6,42 @@
  * @author Nathan Campos <nathan@innoveworkshop.com>
  */
 
+"use strict";
+
 /**
  * Toggles a checkbox checked state.
  *
  * @param {string} checkbox_id  The checkbox identifier.
  */
-var toggle_checkbox = function (checkbox_id) {
+function toggle_checkbox(checkbox_id) {
 	var checkbox = $("#" + checkbox_id);
 	checkbox.prop("checked", !checkbox.prop("checked"));
+}
+
+/**
+ * Toggles the strikethrough style of an element.
+ *
+ * @param {element} elem  The element to have the strikethrough class toggled.
+ * @param {event}   event Event handler for the function. Optional.
+ */
+function toggle_strikethrough(elem, event) {
+	$(elem).toggleClass("refdes-crossed");
+	
+	// Block the propagation?
+	if (event !== undefined)
+		event.stopPropagation();
+}
+
+/**
+ * When attached to the "mousedown" event of an element, you won't be able to
+ * highlight the text ONLY when double-clicking it. This is useful for text
+ * elements with "onclick" events.
+ *
+ * @param {event} event The event object of the element to be used to block.
+ */
+function prevent_dblclickhighlight(event) {
+	if (event.detail <= 1)
+		return;
+	
+	event.preventDefault();
 }
--- public/pick.php
+++ public/pick.php
@@ -29,12 +29,16 @@
 				<?php foreach ($category->get_components() as $component) { ?>
 					<?php $chk_id = 'chk-comp-' . $index++; ?>
 
-					<tr onclick="toggle_checkbox('<?= $chk_id ?>')">
+					<tr onclick="toggle_checkbox('<?= $chk_id ?>')" onmousedown="prevent_dblclickhighlight(event)">
 						<td class="col-1 text-center"><input id="<?= $chk_id ?>" type="checkbox"></td>
 						<td class="col-1 text-center"><?= $component->get_quantity() * $lot_size ?></td>
 						<th scope="row" class="col-2"><?= $component->get_name() ?></th>
 						<td class="col-1 text-center"><?= $component->get_value() ?></td>
-						<td class="col-4"><?= implode(' ', $component->get_refdes()) ?></td>
+						<td class="col-4">
+							<?php foreach ($component->get_refdes() as $refdes) { ?>
+								<span class="refdes" onclick="toggle_strikethrough(this, event)" onmousedown="prevent_dblclickhighlight(event)"><?= $refdes ?></span>
+							<?php } ?>
+						</td>
 						<td class="col-2"><?= $component->get_description() ?></td>
 						<td class="col-1"><?= $component->get_package() ?></td>
 					</tr>
